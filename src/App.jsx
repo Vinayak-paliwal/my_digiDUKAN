b22f25df5ff4fb0817d6d26341dd575f9b576e93
Vinayak-paliwal <vinayakpaliwal866@gmail.com>
2025-01-21 11:59:47 +0530

    hey change

--- src/App.jsx
+++ src/App.jsx
@@ -3,9 +3,10 @@ import './App.css'
 import Header from './components/Header';
 import Home from './modules/Home';
 import Footer from './components/Footer';
-import {Routes, Route} from 'react-router-dom' 
-import Products from './components/Products';
+import {Routes, Route} from 'react-router-dom';
 import Product from './modules/Product';
+import Products from './modules/Products';
+import CategoryProducts from './modules/CategoryProducts';
 
 function App() {
 
@@ -15,6 +16,9 @@ function App() {
       <Routes>
         <Route exact path="/" element={<Home/>}/>
         <Route exact path="/products/:id" element={<Product/>}/>
+        <Route path="/products" element={<Products/>}/>
+        <Route path="/categories/:name" element={<CategoryProducts/>} />
+        <Route path="*" element={<div>404</div>}/>
       </Routes>
       <Footer/>
     </div>
